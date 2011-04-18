7fd61ce855fb6db081f1fc81770b4e2909bd6532
wga_aj@hotmail.com <wga_aj@hotmail.com@4f5bd6d8-de1a-ab62-f4ae-d8a582a987ba>
2011-04-18 16:00:08 +0000

    admin rails

--- trunk/Ruby/leilao/config/routes.rb
+++ trunk/Ruby/leilao/config/routes.rb
@@ -1,4 +1,6 @@
 Leilao::Application.routes.draw do
+  devise_for :users
+
   resources :bids
 
   resources :concessionaires
@@ -71,6 +73,7 @@ Leilao::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+  root :to => "users#sign_in"
 
   # See how all your routes lay out with "rake routes"
 
